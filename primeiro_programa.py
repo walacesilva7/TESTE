1a56790f374860587bad32a914091b95ec3760b5
walacesilva7 <129289556+walacesilva7@users.noreply.github.com>
2023-03-29 09:55:46 -0300

    Initial commit

---
+++ primeiro_programa.py
@@ -0,0 +1,21 @@
+from collections import deque
+def my_function(fname):
+  print(fname + " Refsnes")
+
+def soma (a):
+    print(a+5)
+
+soma(5)
+
+def dois (a,b):
+    print(a+b)
+
+dois(5,7)
+
+
+
+
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
